04d04e5bded4d8b723f980e499a8bbcb15cec2e7
Cindy Lepatan <143421355+missglasses@users.noreply.github.com>
2025-06-03 20:00:13 +0800

    Add files via upload

---
+++ Review/review-basic.py
@@ -0,0 +1,25 @@
+print("Reviewing the basics of Python")
+
+print("Python calculator")
+
+#Addition
+print(5+5)
+
+#Subtraction
+print(5-4)
+
+#Division
+print(10/2)
+
+#Multipication
+print(5*5)
+
+print("Variables: ")
+
+#Integer
+baon = 10
+print ("Ang bawn nako kay : %d" %baon)
+#String
+word = "This is a string"
+print(word)
+
